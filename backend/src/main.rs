214a3323f2cabdf4493afbbe94d58cf1b616b3eb
VinEckSie <new.riverside751@passmail.net>
2025-07-02 21:51:23 +0200

    feat(DDD): update toml and main

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,28 +1,5 @@
-<<<<<<< HEAD
-// Copyright (c) 2025 Vincent Eckert Sierota
-// Licensed under the MIT License
-
-mod domain;
-mod routes;
-
-use sqlx::postgres::PgPoolOptions;
-use crate::routes::create_router;
-use tower_http::trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer};
-use tracing::Level;
-use tracing_appender::rolling;
-use tracing_subscriber::{EnvFilter, fmt};
-
-struct MySqlPool(&'static str);
-
-impl MySqlPool {
-    async fn connect(p0: &str) {
-        todo!()
-    }
-}
-=======
 // #![deny(missing_docs)]
 use backend::start;
->>>>>>> 0c4557a (feat(DDD): clean structure)
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
