8e35b7d3d6c06511d3693068122016e30bb11425
VinEckSie <new.riverside751@passmail.net>
2025-07-06 19:06:58 +0200

    test ci workflow2

--- backend/src/handlers/health.rs
+++ backend/src/handlers/health.rs
@@ -1,5 +1,5 @@
 use axum::response::IntoResponse;
-use axum::{routing::get, Router};
+use axum::{Router, routing::get};
 
 pub fn router() -> Router {
     Router::new().route("/health", get(health_check))
